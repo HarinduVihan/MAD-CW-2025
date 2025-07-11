cc97b3bffcaf887b749a9e326b22c0013ec64159
HarinduVihan <harinduvihan@gmail.com>
2025-07-11 22:48:21 +0530

    nidid mathai

--- ScienceMore/app/src/main/java/com/example/sciencemore/StudentTeacherLogin.java
+++ ScienceMore/app/src/main/java/com/example/sciencemore/StudentTeacherLogin.java
@@ -84,8 +84,7 @@ public class StudentTeacherLogin extends AppCompatActivity {
                         if (querySnapshot != null && !querySnapshot.isEmpty()) {
                             Toast.makeText(this, "Login success as a Teacher", Toast.LENGTH_SHORT).show();
                             //open Teacher dashboard
-                            // startActivity(new Intent(AdminLogin.this,TeacherDashboard.class));
-                            startActivity(new Intent(StudentTeacherLogin.this, MainActivity.class));
+                            startActivity(new Intent(StudentTeacherLogin.this,TeacherDashboard.class));
                         } else {
                             Toast.makeText(this, "Enter correct username and password", Toast.LENGTH_SHORT).show();
                         }
